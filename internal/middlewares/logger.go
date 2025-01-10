2f8c4f15e40f8ee1f2d741013a4dfe2b7e6a657e
Евгений <kirillin.e@etton.ru>
2025-01-10 14:35:05 +0300

    Рефакторинг. Правки логирования.

--- internal/middlewares/logger.go
+++ internal/middlewares/logger.go
@@ -3,7 +3,6 @@ package middlewares
 import (
 	"go.uber.org/zap"
 	"net/http"
-	"strconv"
 	"time"
 )
 
@@ -12,22 +11,22 @@ func Logger(logger *zap.Logger) func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			start := time.Now()
 
-			ww := &responseWriter{ResponseWriter: w, status: http.StatusOK, size: 0}
+			ww := &responseWriter{ResponseWriter: w, status: http.StatusOK}
+
+			defer func() {
+				// Логируем запрос
+				logger.Info("Request",
+					zap.String("method", r.Method),
+					zap.String("uri", r.RequestURI),
+					zap.Int("status", ww.status),
+					zap.Int("size", ww.size),
+					zap.Float64("duration", time.Since(start).Seconds()),
+					zap.String("remote_addr", r.RemoteAddr),
+					zap.String("user_agent", r.UserAgent()),
+				)
+			}()
 
 			next.ServeHTTP(ww, r)
-
-			ww.status, _ = strconv.Atoi(w.Header().Get("Status-URI"))
-
-			// Логируем запрос
-			logger.Info("Request",
-				zap.String("method", r.Method),
-				zap.String("uri", r.RequestURI),
-				zap.Int("status", ww.status),
-				zap.Int("size", ww.size),
-				zap.Float64("duration", time.Since(start).Seconds()),
-				zap.String("remote_addr", r.RemoteAddr),
-				zap.String("user_agent", r.UserAgent()),
-			)
 		})
 	}
 }
